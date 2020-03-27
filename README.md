d782b5afba1ed8dc0b8c94eec6c2dcb0f03a6eb1
toonsegers <a.j.m.segers@tue.nl>
2020-03-27 10:01:48 +0100

    update license and readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # zk_helpers
-Helpers for R1CS and QAP construction, and bn256 pairings
+
+Python helper modules for constructing ZK proof systems using R1CS, QAP and pairings.
+
+## Contents 
+
+Repository contains helpers for:
+
+* Conversion of code to Rank 1 Constraint System (R1CS)
+* Conversion of R1CS to Quadratic Arithmetic Program (QAP)
+* Pairing operations based on 256-bit Barreto-Naehrig (bn256) curve
+
+## Credits
+
+This is derived work. For copyright and license information of the original
+works see LICENSE file. Links to original repositories:
+
+* bn256 by Jack Lloyd: https://github.com/randombit/pairings.py/ (BSD-2-Clause license)
+* r1cs and qap tools by Vitalik Buterin: https://github.com/ethereum/research/tree/master/zksnark (MIT license)
+
+
---
+++ pairings/bn256_to_multiplicative.py
@@ -0,0 +1,57 @@
+""" Wrapper to represent bn256 groups as multiplicative groups.
+
+CurvePoint and CurveTwist classes from bn256 are overwritten.
+
+Copyright (c) 2020 Toon Segers 
+"""    
+
+
+import sys, os
+project_root = sys.path.append(os.path.abspath('..')) 
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from ac20_circuit_sat.bn256 import *
+
+
+
+class CurvePointMult(CurvePoint):
+    def __add__(self, other): 
+        return NotImplementedError("Addition not defined in multiplicative notation.")
+
+    def __mul__(self, other): 
+        return self.add(other)
+
+    def __pow__(self, scalar): 
+        # return self.scalar_mul(scalar)
+        return self.scalar_mul(int(scalar))
+
+
+
+# Any point (1,y) where y is a square root of b+1 is a generator
+curve_G = CurvePointMult(GFp_1(1), GFp_1(p-2))
+
+assert curve_G.is_on_curve()
+
+
+class CurveTwistMult(CurveTwist):
+    def __add__(self, other): 
+        return NotImplementedError("Addition not defined in multiplicative notation.")
+
+    def __mul__(self, other): 
+        return self.add(other)
+
+    def __pow__(self, scalar): 
+        # return self.scalar_mul(scalar)
+        return self.scalar_mul(int(scalar))
+
+
+# TODO derive this
+twist_G = CurveTwistMult(
+    GFp_2(21167961636542580255011770066570541300993051739349375019639421053990175267184,
+          64746500191241794695844075326670126197795977525365406531717464316923369116492),
+    GFp_2(20666913350058776956210519119118544732556678129809273996262322366050359951122,
+          17778617556404439934652658462602675281523610326338642107814333856843981424549),
+    GFp_2(0,1))
+
+assert twist_G.is_on_curve()
